02babe89dffa8834246c7907c5ba8c04dd6f026f
ykrueng <yasirah.krueng@gmail.com>
2018-12-20 17:05:08 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
+const _ = require("lodash");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -47,6 +48,29 @@ app.delete(`/todos/:id`, (req, res) => {
   Todo.findByIdAndRemove(id)
     .then(todo => {
       return !todo ? res.status(404).send() : res.send({ todo });
+    })
+    .catch(e => res.status(400).send());
+});
+
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
     })
     .catch(e => res.status(400).send());
 });
